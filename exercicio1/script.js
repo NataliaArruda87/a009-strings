f85130c86edc49c6e3ec23780528adce808afbe0
NataliaArruda87 <natalia.arruda87@gmail.com>
2022-08-31 16:31:13 -0300

    Resolução dos Exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+let nome = prompt("Qual é o seu nome?")
+let comida1 = prompt("Qual o nome da sua comida favorita?")
+let comida2 = prompt("Qual o nome da sua segunda comida favorita?")
+let comida3 = prompt("Qual o nome da sua terceira comida favorita?")
+
+console.log(`
+Estas são as comidas favoritas de ${nome}:
+- ${comida1}
+- ${comida2}
+- ${comida3}
+
+`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,5 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      "
+console.log(minhaString)
+console.log(`Quantidade de caracteres = ${minhaString.length}`)
+console.log(`Quantidade de caracteres após remoção dos espaços = ${minhaString.trim().length}`)
+console.log(minhaString.replace("________", "sticioso"))
